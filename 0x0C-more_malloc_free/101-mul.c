9ae8f790c2eb9189dad14301141e980df38fc3a5
Lorna Ndathe <lornahndathe@gmail.com>
2023-06-15 17:34:44 -0700

    more malloc free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,7 +1,54 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
+
+#define ERR_MSG "Error"
+
+/**
+ * is_digit - confirms string has digits char
+ * @s: string to be confirmed
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int is_digit(char *s)
+{
+	int a = 0;
+
+	while (s[a])
+	{
+		if (s[a] < '0' || s[a] > '9')
+			return (0);
+		a++;
+	}
+	return (1);
+}
+
+/**
+ * _strlen -  length of  string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	return (a);
+}
+
+/**
+ * errors - checks errors in  main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
 
 /**
  * main - product of two positive numbers
@@ -12,27 +59,45 @@
 
 int main(int argc, char *argv[])
 {
-	unsigned long mul;
-	int a, b;
+	char *a, *b;
+	int msure1, msure2, msure, c, carry, no1, no2, *product, d = 0;
 
-	if (argc != 3)
+	a = argv[1], b = argv[2];
+	if (argc != 3 || !is_digit(a) || !is_digit(b))
+		errors();
+	msure1 = _strlen(a);
+	msure2 = _strlen(b);
+	msure = msure1 + msure2 + 1;
+	product = malloc(sizeof(int) * msure);
+	if (!product)
+		return (1);
+	for (c = 0; c <= msure1 + msure2; c++)
+		product[c] = 0;
+	for (msure1 = msure1 - 1; msure1 >= 0; msure1--)
 	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	for (a = 1; a < argc; a++)
-	{
-		for (b = 0; argv[a][b] != '\0'; b++)
+		no1 = a[msure1] - '0';
+		carry = 0;
+		for (msure2 = _strlen(b) - 1; msure2 >= 0; msure2--)
 		{
-			if (argv[a][b] > 57 || argv[a][b] < 48)
-			{
-				printf("Error\n");
-				exit(98);
-			}
+			no2 = b[msure2] - '0';
+			carry += product[msure1 + msure2 + 1] + (no1 * no2);
+			product[msure1 + msure2 + 1] = carry % 10;
+			carry /= 10;
 		}
+		if (carry > 0)
+			product[msure1 + msure2 + 1] += carry;
 	}
-	mul = atol(argv[1]) * atol(argv[2]);
-	printf("%lu\n", mul);
+	for (c = 0; c < msure - 1; c++)
+	{
+		if (product[d])
+			d = 1;
+		if (d)
+			_putchar(product[d] + '0');
+	}
+	if (!d)
+		_putchar('0');
+	_putchar('\n');
+	free(product);
 	return (0);
 }
+
