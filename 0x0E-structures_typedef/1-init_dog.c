41f8eb45e7448f0e0151a77b678256089e34b6a5
Lorna Ndathe <lornahndathe@gmail.com>
2023-06-20 16:20:10 -0700

    structures_typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,17 +8,14 @@
  * @age: pointer to the age of the dog
  * @owner: pointer to the owner of the dog
  *
- * Return: void
- *
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-	{ 
-		(*d).name = name;
-		(*d).age = age;
-		d->owner = owner;
-	}
-
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
+
