58d427eafce39e47842b37929a51125eb596d139
Lorna Ndathe <lornahndathe@gmail.com>
2023-06-22 16:39:50 -0700

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - Prints numbers, followed by line.
+ * @separator: pointer  printed between numbers.
+ * @n amount of integers passed
+ * @...: A variable number of numbers to be printed
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int no;
+
+	va_start(list, n);
+
+	for (no = 0; no < n; no++)
+	{
+		printf("%d", va_arg(list, int));
+
+		if (no != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+
+	va_end(list);
+}
+
